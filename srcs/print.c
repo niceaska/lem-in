c71456ec9a1a56df03a436234dbb69226fc07489
niceaska <niceaska@gmail.com>
2019-06-20 18:06:08 +0300

    fix issue with wrong start ant index

--- srcs/print.c
+++ srcs/print.c
@@ -6,7 +6,7 @@
 /*   By: lgigi <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/15 14:49:43 by lgigi             #+#    #+#             */
-/*   Updated: 2019/06/19 19:28:20 by lgigi            ###   ########.fr       */
+/*   Updated: 2019/06/20 18:04:43 by lgigi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,7 +73,8 @@ void			print_moves(t_node **p_arr, t_bfs *bs, int i, int j)
 		while (++i < bs->ants)
 			if (ants[i] && ants[i]->next && rooms[CURRIND(ants[i])] && \
 			(!rooms[NXTIND(ants[i])] || (NXTIND(ants[i]) == END)))
-				if (print_ant_move(rooms, ants, i, j) && ants[i]\
+				if (st_moves_ch(ants, rooms, bs, &i) &&\
+					print_ant_move(rooms, ants, i, j) && ants[i]\
 				&& !ants[i]->next && NXTIND(ants[i + 1]) == END)
 					break;
 		ft_printf("\n");
--- srcs/utils.c
+++ srcs/utils.c
@@ -6,19 +6,22 @@
 /*   By: lgigi <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/17 22:32:55 by lgigi             #+#    #+#             */
-/*   Updated: 2019/06/19 22:15:20 by lgigi            ###   ########.fr       */
+/*   Updated: 2019/06/20 18:05:07 by lgigi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lemmin.h"
 
 int ft_addedge(t_node **arr, t_data *start, t_data *end) 
-{ 
+{
+	int ret;
+
+	ret = 1;
 	if (!(push_back(&(arr[start->index]), end, 1)))
-		return (0);
+		ret--;
 	if (!(push_back(&(arr[end->index]), start, 1)))
-		return (0);
-	return (1); 
+		ret--;
+	return ((ret < 0) ? 0 : ret); 
 }
 
 t_data	*init_data(char *name, int index)
@@ -44,4 +47,26 @@ int		*init_rooms(int vrt, int start_index, int ants)
 		rooms[i++] = 0;
 	rooms[start_index] = ants;
 	return (rooms);
+}
+
+int			st_moves_ch(t_node **ants, int *rooms, t_bfs *bs, int *i)
+{
+	int		j;
+	t_node	*temp;
+
+	j = 0;
+	if (ants[*i] && CURRIND(ants[*i]) == START)
+		while (ants[j] && j != *i && (CURRIND(ants[j]) != START\
+				|| !rooms[NXTIND(ants[j])] || \
+				!ants[j]->next || NXTIND(ants[j]) == END))
+				j++;
+	if (j < *i && ants[j] && j != 0)
+	{
+		
+		temp = ants[*i];
+		ants[*i] = ants[j];
+		ants[j] = temp;
+		*i = j;
+	}
+	return (1);
 }
